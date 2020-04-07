19593d08501b96b8728cd5c04c4d299e2eb63331
Jaseemck <jaseemckclt@gmail.com>
2020-04-07 20:06:47 +0530

    Day-1:Basic_Template

--- application.py
+++ application.py
@@ -1,10 +1,26 @@
-from flask import Flask 
+from flask import Flask, render_template
+from newsapi import NewsApiClient
 
 application = Flask(__name__)
 
 @application.route("/")
 def homepage():
-    return("Hello World!")
+    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
+    #topheadlines = newsapi.get_everything(sources='bbc-news')
+    topheadlines = newsapi.get_top_headlines(q='covid',
+                                      country='in',
+                                      language='en')
+    articles = topheadlines['articles']
+    desc=[]
+    news=[]
+    img=[]
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+    mylist = zip(news,desc,img)
+    return render_template('index.html',context=mylist)
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
