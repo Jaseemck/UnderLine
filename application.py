abee7f1f17b4a11a754cbff37dccfcebd8f30a9a
Jaseemck <jaseemckclt@gmail.com>
2020-04-18 08:20:38 +0530

    Added images and styling

--- application.py
+++ application.py
@@ -43,7 +43,7 @@ def cov():
     newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
     #topheadlines = newsapi.get_everything(sources='bbc-news')
     topheadlines = newsapi.get_everything(q='covid',
-                                      language='en')
+                                    language='en')
     articles = topheadlines['articles']
     desc=[]
     news=[]
@@ -56,14 +56,32 @@ def cov():
     mylist = zip(news,desc,img)
     return render_template('covid.html',context=mylist)
 
+@application.route("/biz")
+def business():
+    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
+    #topheadlines = newsapi.get_everything(sources='bbc-news')
+    topheadlines = newsapi.get_everything(q='business',
+                                    language='en')
+    articles = topheadlines['articles']
+    desc=[]
+    news=[]
+    img=[]
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+    mylist = zip(news,desc,img)
+    return render_template('biz.html',context=mylist)
+
 @application.route("/world")
 def ever():
     newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
     #topheadlines = newsapi.get_everything(sources='bbc-news')
     all_articles = newsapi.get_everything(
-                                      sources='bbc-news,the-verge',
-                                      language='en',
-                                      sort_by='relevancy')
+                                    sources='bbc-news,the-verge',
+                                    language='en',
+                                    sort_by='relevancy')
     articles = all_articles['articles']
     desc=[]
     news=[]
