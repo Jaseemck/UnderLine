5eafdea329cc7b2988e16d15705e39a8f309b585
Jaseemck <jaseemckclt@gmail.com>
2020-05-21 18:10:06 +0530

    optimized

--- main.py
+++ main.py
@@ -1,83 +1,82 @@
 from flask import Flask, render_template
 from newsapi import NewsApiClient
 
-application = Flask(__name__)
+app = Flask(__name__)
+newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
 
-@application.route("/")
+@app.route("/")
 def homepage():
-    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
-    #topheadlines = newsapi.get_everything(sources='bbc-news')
     topheadlines = newsapi.get_top_headlines(language='en')
     articles = topheadlines['articles']
     desc=[]
     news=[]
     img=[]
+    url=[]
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
-    mylist = zip(news,desc,img)
+        url.append(myarticles['url'])
+    mylist = zip(news,desc,img,url)
     return render_template('index.html',context=mylist)
 
-@application.route("/in")
+@app.route("/in")
 def ind():
-    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
-    #topheadlines = newsapi.get_everything(sources='bbc-news')
     topheadlines = newsapi.get_top_headlines(country='in',
                                       language='en')
     articles = topheadlines['articles']
     desc=[]
     news=[]
     img=[]
+    url=[]
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
-    mylist = zip(news,desc,img)
+        url.append(myarticles['url'])
+    mylist = zip(news,desc,img,url)
     return render_template('india.html',context=mylist)
 
-@application.route("/covid")
+@app.route("/covid")
 def cov():
-    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
-    #topheadlines = newsapi.get_everything(sources='bbc-news')
     topheadlines = newsapi.get_everything(q='covid',
                                     language='en')
     articles = topheadlines['articles']
     desc=[]
     news=[]
     img=[]
+    url=[]
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
-    mylist = zip(news,desc,img)
+        url.append(myarticles['url'])
+    mylist = zip(news,desc,img,url)
     return render_template('covid.html',context=mylist)
 
-@application.route("/biz")
+@app.route("/biz")
 def business():
-    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
-    #topheadlines = newsapi.get_everything(sources='bbc-news')
     topheadlines = newsapi.get_everything(q='business',
                                     language='en')
     articles = topheadlines['articles']
     desc=[]
     news=[]
     img=[]
+    url=[]
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
-    mylist = zip(news,desc,img)
+        url.append(myarticles['url'])
+    mylist = zip(news,desc,img,url)
     return render_template('biz.html',context=mylist)
 
-@application.route("/world")
+@app.route("/world")
 def ever():
-    newsapi = NewsApiClient(api_key="86169c5e6d3f48da8dc3f9402c6efbb9")
-    #topheadlines = newsapi.get_everything(sources='bbc-news')
     all_articles = newsapi.get_everything(
                                     sources='bbc-news,the-verge',
                                     language='en',
@@ -86,13 +85,15 @@ def ever():
     desc=[]
     news=[]
     img=[]
+    url=[]
     for i in range(len(articles)):
         myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
         img.append(myarticles['urlToImage'])
-    mylist = zip(news,desc,img)
+        url.append(myarticles['url'])
+    mylist = zip(news,desc,img,url)
     return render_template('world.html',context=mylist)
 
 if __name__ == "__main__":
-    application.run(debug=True)
+    app.run(debug=True)
